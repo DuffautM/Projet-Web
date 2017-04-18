dd5cd6dd5fed491671c829173cb19142cdccfadb
julien-mazzia <julien.mazzia@viacesi.fr>
2017-04-18 16:17:21 +0200

    Fiche_club V1.0

---
+++ site_web/resources/views/fiche_club.blade.php
@@ -0,0 +1,12 @@
+@extends('template/template_club')
+@section('titre')
+<div class="container">
+    <div class="row">
+        <h3 class="title">Nom du club</h3>
+    </div>
+@endsection
+
+
+@section('img1')
+    "{!!asset('img/Club.jpg')!!}"
+@endsection
