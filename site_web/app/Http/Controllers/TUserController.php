3d98c17d48c8bbbde79dd74900ba1218c98a8b05
DUFFAUTM <maxime.duffaut1@orange.fr>
2017-04-20 13:51:26 +0200

    Création cookie
    Création cookie contenant l'id de l'utilisateur se connectant

--- site_web/app/Http/Controllers/TUserController.php
+++ site_web/app/Http/Controllers/TUserController.php
@@ -19,13 +19,14 @@ class TUserController extends Controller
 
     public function login(Request $request){
 
-        //dd($user);
-        //return view('profil', compact('user'));
+        $users_id = \DB::table('t_users')->where('mail_user', '=', $request->InputMail)->first()->id;
 
         if(TUser::where('mail_user', $request->InputMail)->where('password_user', $request->InputPassword)->first())
         {
 
-                return view('correct_login');
+            \Cookie::queue('id', (string)$users_id, 3600);
+
+            return view('correct_login');
 
         }
 
