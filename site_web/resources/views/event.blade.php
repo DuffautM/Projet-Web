3873c9f4ee9b79c8cd8ee986960af0deb1a37cc4
julien-mazzia <julien.mazzia@viacesi.fr>
2017-04-20 10:18:19 +0200

    Page_event 2.0

--- site_web/resources/views/event.blade.php
+++ site_web/resources/views/event.blade.php
@@ -10,15 +10,17 @@
 @section('contenu')
     <div class="row">
         <h4>Prochainement :</h4>
-        <img src="{!!asset('img/Event.jpg')!!}" class="img-responsive col-xs-3">
-        <img src="{!!asset('img/Event.jpg')!!}" class="img-responsive col-xs-3">
-        <img src="{!!asset('img/Event.jpg')!!}" class="img-responsive col-xs-3">
+        @foreach($event as $ev)
+		<img src="{!!asset('img/Event.jpg')!!}" class="img-responsive col-xs-3">
+
+	   @endforeach
     </div>
     <div class="row">
         <h4>Réalisé :</h4>
-        <img src="{!!asset('img/Event.jpg')!!}" class="img-responsive col-xs-3">
-        <img src="{!!asset('img/Event.jpg')!!}" class="img-responsive col-xs-3">
-        <img src="{!!asset('img/Event.jpg')!!}" class="img-responsive col-xs-3">
+        @foreach($past as $pass)
+		<img src="{!!asset('img/Event.jpg')!!}" class="img-responsive col-xs-3">
+
+	   @endforeach
     </div>
     </div>
 @endsection
